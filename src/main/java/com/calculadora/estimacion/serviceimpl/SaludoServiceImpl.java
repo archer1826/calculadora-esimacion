9bb3f92f08752717bf7440c2fc8adc02a2a9e270
Duvan Rodriguez <duvanpenagos1002@gmail.com>
2023-05-27 13:23:03 -0500

    Duvan Rodriguez, crecion de bd mysql con tabla saludo

--- src/main/java/com/calculadora/estimacion/serviceimpl/SaludoServiceImpl.java
+++ src/main/java/com/calculadora/estimacion/serviceimpl/SaludoServiceImpl.java
@@ -1,18 +1,23 @@
 package com.calculadora.estimacion.serviceimpl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import com.calculadora.estimacion.dto.ApiResponse;
+import com.calculadora.estimacion.repository.ISaludoRepository;
 import com.calculadora.estimacion.service.ISaludoService;
 import com.calculadora.estimacion.util.ResponseUtil;
 
 @Service
 public class SaludoServiceImpl implements ISaludoService {
 
+	@Autowired
+	private ISaludoRepository repository;
+	
 	@Override
 	public ApiResponse saludo() {
-		return ResponseUtil.response(HttpStatus.OK, "bien", "HOLA MUNDO");
+		return ResponseUtil.response(HttpStatus.OK, "bien", repository.findAll());
 	}
 
 
