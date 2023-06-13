bed10387e605d0e49457033c8c5d20cd51f0a748
usera <usera@dockera>
2023-06-12 21:21:50 -0500

    Crea la tabla Fabrica con sus campos

---
+++ src/main/java/com/calculadora/estimacion/repository/FabricaRepository.java
@@ -0,0 +1,8 @@
+package com.calculadora.estimacion.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.calculadora.estimacion.entity.Fabrica;
+
+public interface FabricaRepository extends JpaRepository<Fabrica, Integer> {
+
+}
